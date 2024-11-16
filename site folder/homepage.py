a479cd3b3e3d3b6af6be4ac57cb1d9d939fa14c6
Sarah <89218700+sarahnzhou@users.noreply.github.com>
2024-11-16 02:31:00 -0500

    buttons

--- site folder/homepage.py
+++ site folder/homepage.py
@@ -4,7 +4,14 @@ from nicegui import ui
 #organizations = []
 #userData = {"role": None, "name": "", "address": "", "resources": [], "days": None}
 
-#def homepage():
-ui.button('Company', on_click=lambda: ui.open('https://www.google.com/maps'))
+#mainpage
+
+@ui.page('/other_page')
+def other_page():
+    ui.label('this is the other page')
+
+with ui.button_group():
+    ui.button('Company', on_click=lambda: ui.navigate.to('/other_page'), color = 'yellow').style('font-size: 100px').props('outline')
+    ui.button('Organization', on_click=lambda: ui.navigate.to('/other_page')).style('font-size: 100px')
 
 ui.run()
