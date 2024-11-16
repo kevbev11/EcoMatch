26eff73fd4bdf548b71388506d0d0f4284e8c18d
Kevin <kktang@andrew.cmu.edu>
2024-11-16 02:31:08 -0500

    profile page

---
+++ site folder/profiles.py
@@ -0,0 +1,25 @@
+from nicegui import ui
+
+ui.input(label='Name', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='Email', placeholder='example@gmail.com',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='Phone Number', placeholder='(XXX) XXX-XXXX',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='Address', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='City', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='State', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='Zip Code', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='When?', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+ui.input(label='How many?', placeholder='start typing',
+         on_change=lambda e: result.set_text('you typed: ' + e.value))
+
+
+result = ui.label()
+
+ui.run()
