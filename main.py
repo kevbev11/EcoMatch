811b9cae208834ec582310e322489fded54f9078
Kevin <kktang@andrew.cmu.edu>
2024-11-15 23:27:29 -0500

    user input updates

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ class Company:
             self.resources.append((resources[i], quantity[i]))
         self.time = time
 
+    def __repr__(self):
+        resources = ''
+        for r, q in self.resources:
+            resources = resources + f'{q} ' + r + ', '
+        resources = resources[:len(resources) - 2]
+        return f'{self.name} at {self.location} can provide {resources} in {self.time} days'
+
 class Organization:
     def __init__(self, name, location, resources, quantity, time):
         self.name = name
@@ -15,6 +22,13 @@ class Organization:
         for i in range(len(resources)):
             self.resources.append((resources[i], quantity[i]))
         self.time = time
+
+    def __repr__(self):
+        resources = ''
+        for r, q in self.resources:
+            resources = resources + f'{q} ' + r + ', '
+        resources = resources[:len(resources) - 2]
+        return f'{self.name} at {self.location} needs {resources} in {self.time} days'
 
 #picks between company or charity
 def getType():
@@ -36,19 +50,51 @@ def getType():
 
 #returns the object
 def getInfo(type):
+    addResources = True
+    resource = []
+    quantity = []
     if type == "Company":
-        name = input("What is your company called?")
-        location = input("What is your company's address?")
-        resource = [input("What resource can you provide?")]
-        quantity = [input("How many can you provide?")]
-        time = [int(input("In how many days can you deliver this item (type a number)?"))]
+        name = input("What is your company called? ")
+        location = input("What is your company's address? ")
+        while addResources:
+            resource.append(input("What resource can you provide? "))
+            while True:
+                try:
+                    quantity.append(int(input("How many can you provide (type a number)? ")))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a number.")   
+            check = input("Would you like to add another resource? [Y]/[N] ")
+            if check == 'N':
+                addResources = False
+        while True:
+            try:
+                time = int(input("In how many days can you deliver these items (type a number)? "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
         return Company(name, location, resource, quantity, time)
+    
     else:
-        name = input("What is your organization called?")
-        location = input("What is your organization's address?")
-        resource = [input("What resource do you need?")]
-        quantity = [int(input("How many do you need?"))]
-        time = [int(input("In how many days do you need it by (type a number)?"))]
+        name = input("What is your organization called? ")
+        location = input("What is your organization's address? ")
+        while addResources:
+            resource.append(input("What resource do you need? "))
+            while True:
+                try:
+                    quantity.append(int(input("How many do you need (type a number)? ")))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+            check = input("Would you like to add another resource? [Y]/[N] ")
+            if check == 'N':
+                addResources = False
+        while True:
+            try:
+                time = int(input("In how many days do you need these by (type a number)? "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
         return Organization(name, location, resource, quantity, time)
 
 def main():
