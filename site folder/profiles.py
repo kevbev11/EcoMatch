f7033fdc5aee7f65659eaf5e8a83b573fdf17c7b
Kevin <kktang@andrew.cmu.edu>
2024-11-16 02:33:35 -0500

    profiles

--- site folder/profiles.py
+++ site folder/profiles.py
@@ -8,9 +8,9 @@ ui.input(label='Phone Number', placeholder='(XXX) XXX-XXXX',
          on_change=lambda e: result.set_text('you typed: ' + e.value))
 ui.input(label='Address', placeholder='start typing',
          on_change=lambda e: result.set_text('you typed: ' + e.value))
-ui.input(label='City', placeholder='start typing',
+ui.input(label='City', placeholder='ex. Danville',
          on_change=lambda e: result.set_text('you typed: ' + e.value))
-ui.input(label='State', placeholder='start typing',
+ui.input(label='State', placeholder='ex. CA',
          on_change=lambda e: result.set_text('you typed: ' + e.value))
 ui.input(label='Zip Code', placeholder='start typing',
          on_change=lambda e: result.set_text('you typed: ' + e.value))
